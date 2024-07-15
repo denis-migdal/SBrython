52d1e06f79433d6655130a4f7d21cbcde20dcbfe
denmigda <denis.migdal@uca.fr>
2024-07-15 16:33:38 +0200

    Simplifies fctcall

--- src/core_modules/fctcall/ast2js.ts
+++ src/core_modules/fctcall/ast2js.ts
@@ -4,20 +4,26 @@ import { ASTNode } from "structs/ASTNode";
 export default function ast2js(this: ASTNode) {
 
     let cursor = {...this.jscode!.start};
-    const start_col = cursor.col;
 
     let js = `${ astnode2js(this.children[0], cursor)}(`;
+    cursor.col += 1;
 
     for(let i = 1; i < this.children.length; ++i) {
 
-        if( i !== 1)
+        if( i !== 1) {
             js += ",";
+            cursor.col += 1;
+        }
         
-        cursor.col = start_col + js.length;
         js += `${ astnode2js(this.children[i], cursor)}`;
     }
 
     js += ")";
+
+    this.jscode!.end = {
+        line: cursor.line,
+        col : cursor.col + 1,
+    }
 
     return js;
 }
