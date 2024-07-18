c368af90718222c053b18115d3f019caad4eae3c
denmigda <denis.migdal@uca.fr>
2024-07-18 14:52:34 +0200

    Add elif

--- src/core_modules/controlflows/ifblock/ast2js.ts
+++ src/core_modules/controlflows/ifblock/ast2js.ts
@@ -5,15 +5,29 @@ export default function ast2js(this: ASTNode) {
 
     let cursor = {...this.jscode!.start};
 
-    if( this.type === "controlflows.ifblock")
-        return astnode2js(this.children[0], cursor);
+    if( this.type === "controlflows.ifblock") {
+        let js = "";
+        for(let i = 0; i < this.children.length; ++i)
+            js += astnode2js(this.children[i], cursor);
+        return js;
+    }
 
     //if
-    let js = "if(";
+    let keyword = "if";
+    if( this.type === "controlflows.elif")
+        keyword = "else if";
+
+    let js = `${keyword}(`;
     cursor.col += js.length;
     js += astnode2js(this.children[0], cursor);
     js += ")";
+    if(keyword !== "if") {  // h4ck
+        --this.jscode!.start.col;
+    }
     js += body2js(this, cursor, 1);
+    if(keyword !== "if") {  // h4ck
+        ++this.jscode!.start.col;
+    }
 
     this.jscode!.end = {...cursor};
 
--- src/core_modules/controlflows/ifblock/astconvert.ts
+++ src/core_modules/controlflows/ifblock/astconvert.ts
@@ -1,34 +1,49 @@
 import { Context, convert_body, convert_line, convert_node } from "py2ast";
 import { ASTNode } from "structs/ASTNode";
 
-//TODO: better system...
-let is_if = false;
-
 export default function convert(node: any, context: Context) {
 
     if( ! ("test" in node) )
         return;
 
-    if( is_if ) {
-        is_if = false;
+    console.log(node);
+
+    if( node.ifblock === "if" || node.ifblock === "elif" ) {
 
         const cond = convert_node(node.test, context);
-
-        if( node.orelse.length !== 0)
-            throw new Error("else/elif not yet supported");
         
         if(cond.result_type !== "bool")
             throw new Error(`Type ${cond.result_type} not yet supported as if condition`);
 
-        return new ASTNode(node, "controlflows.if", null, null, [
+        return new ASTNode(node, `controlflows.${node.ifblock}`, null, null, [
             cond,
             ...convert_body(node, context)
         ]);
     }
 
-    is_if = true;
+    node.ifblock = "if";
 
-    return new ASTNode(node, "controlflows.ifblock", null, null, [
-            convert_node(node, context)
+    const children = [
+        node
+    ];
+
+    let cur = node; // else is different
+    while( "orelse" in cur && cur.orelse.length > 0) {
+
+        // if test = elif / no test = else.
+        cur = cur.orelse[0];
+        cur.ifblock = "elif";
+        children.push(cur);
+    }
+
+    const astnode = new ASTNode(node, "controlflows.ifblock", null, null, [
+            ...children.map( n => convert_node(n, context) )
         ]);
+    
+    for(let i = 0; i < astnode.children.length-1; ++i) {
+        const cc = astnode.children[i].children;
+        astnode.children[i].pycode.end = cc[cc.length-1].pycode.end;
+    }
+
+    return astnode;
 }
